476848f525054f7cdf52d8133d066def1aa6afd8
Mark Haley <77818787+maha4208@users.noreply.github.com>
2023-03-16 13:51:38 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String[] args) {
         // bootstrap code only - no logic
         Simulator sim = new Simulator();
-        sim.run();  // Let's do this thing
+        sim.runSimulations();  // Let's do this thing
     }
 }
